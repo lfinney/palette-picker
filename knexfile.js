ee145cf2ed442552adbd724360471f08b94f279b
Luke Finney <lucaswfinney@gmail.com>
2017-12-01 10:23:40 -0700

    heroku functionality changes

--- knexfile.js
+++ knexfile.js
@@ -23,5 +23,13 @@ module.exports = {
       directory: './db/seeds/test'
     },
     useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 };
